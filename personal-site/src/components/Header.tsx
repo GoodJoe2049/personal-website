5e51cd1705abe4b01cc7940a40dace44a686d501
GoodJoe2049 <marcosrod171@gmail.com>
2023-07-02 19:23:39 -0500

    setup emotion

---
+++ personal-site/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const DummyCSS = styled.div`
+
+`;
+
+const Header = () => {
+    //header to be implemented
+    return (
+        <h1>hi</h1>
+    );
+};
+
+export default Header;
